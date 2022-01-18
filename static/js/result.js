e6d6c40695411f1531ecc43bd14c62d815342ca3
Yoon Sohyeon <ysh0714@naver.com>
2022-01-18 12:21:45 +0900

    업로드 - 최종 연동

---
+++ static/js/result.js
@@ -0,0 +1,62 @@
+$(document).ready(function () {
+    show_ranking();
+});
+
+function logout() {
+    $.removeCookie('mytoken');
+    alert('로그아웃!')
+    window.location.href = '/login'
+}
+
+// 랭킹 보여주는 함수
+function show_ranking() {
+    $.ajax({
+        type: 'GET',
+        url: '/api/result',
+        data: {},
+        success: function (response) {
+            let rows = response['ranking']
+            rows.sort(function (a,b){
+                return b.score - a.score
+            });
+
+
+            // 1등 아이디, 점수
+            let first_id = rows[0]['id'];
+            let first_score = rows[0]['score'];
+            let temp_html1 = `<h1>1등</h1>
+                            <h1>${first_id}</h1>
+                            <h3>${first_score}점</h3>`
+                $('#first').append(temp_html1)
+
+            // 2등 아이디, 점수
+            let second_id = rows[1]['id'];
+            let second_score = rows[1]['score'];
+            let temp_html2 = `<h3>2등</h3>
+                            <h2>${second_id}</h2>
+                            <h4>${second_score}점</h4>`
+                $('#second').append(temp_html2)
+
+            // 3등 아이디, 점수
+            let third_id = rows[2]['id'];
+            let third_score = rows[2]['score'];
+            let temp_html3 = `<h3>3등</h3>
+                            <h2>${third_id}</h2>
+                            <h4>${third_score}점</h4>`
+                $('#third').append(temp_html3)
+
+
+            for (let i = 3; i < 6; i++) {
+                let id = rows[i]['id'];
+                let score = rows[i]['score'];
+                let temp_html = `<tr>
+                                    <td>${id}</td>
+                                    <td>${score}점</td>
+                                </tr>`
+                $('#rank').append(temp_html)
+            }
+
+        }
+
+    });
+}
